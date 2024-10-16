148a7177534deb529bed4f370db2c3cb559da40c
Ariel <ariel.pintof@userena.cl>
2024-10-16 16:37:09 -0300

    add image upload to template component

---
+++ Utils/StringUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorForms.Utils;
+
+public static class StringUtils
+{
+    public static bool IsExtensionAllowed(this string extension)
+    {
+        if (string.IsNullOrEmpty(extension))
+            throw new ArgumentException("The extension cannot be null or empty.", nameof(extension));
+
+        if (extension[0] != '.')
+            throw new ArgumentException("The extension must start with a '.' character.", nameof(extension));
+        
+        return extension.Equals(".png") || extension.Equals(".jpg") || extension.Equals(".jpeg");
+    }
+}
