306b83452c06d6ae0b6af24d9cbb7add44051200
Ariel <ariel.pintof@userena.cl>
2024-10-16 16:36:12 -0300

    create role service

---
+++ Data/Services/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorForms.Data.Services;
+
+public interface IRoleService
+{
+    Task Create([Required] string name);
+    Task AssignRoleToUser(string userId, string roleName);
+}
---
+++ Data/Services/RoleService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorForms.Data.Services;
+
+public class RoleService : IRoleService
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public RoleService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+    {
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+
+    public async Task Create(string name)
+    {
+        var roleExists = await _roleManager.RoleExistsAsync("admin");
+        if (!roleExists)
+        {
+            var role =await _roleManager.CreateAsync(new IdentityRole("admin"));
+            if (!role.Succeeded) throw new Exception();
+        }
+    }
+    
+    public async Task AssignRoleToUser(string userId, string roleName)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null) throw new Exception("User not found");
+
+        var result = await _userManager.AddToRoleAsync(user, roleName);
+        if (!result.Succeeded) throw new Exception("Could not assign role to user");
+    }
+}
