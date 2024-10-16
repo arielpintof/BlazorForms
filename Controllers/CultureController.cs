6c07fc22c1e3b245d24555b12dc62f17419b55de
Ariel <ariel.pintof@userena.cl>
2024-10-16 02:24:14 -0300

    Update CultureController.cs

--- Controllers/CultureController.cs
+++ Controllers/CultureController.cs
@@ -7,7 +7,7 @@ namespace BlazorForms.Controllers;
 public class CultureController : Controller
 {
     
-    public IActionResult Set(string culture, string redirectUri)
+    public IActionResult Set(string? culture, string redirectUri)
     {
         if (culture is null) return LocalRedirect(redirectUri);
         
