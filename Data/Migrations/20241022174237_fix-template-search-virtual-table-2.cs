4c4e3e6c89381c8d9da83d02e0462d76c035780f
Ariel <ariel.pintof@userena.cl>
2024-10-22 22:57:03 -0300

    add migrations

---
+++ Data/Migrations/20241022174237_fix-template-search-virtual-table-2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorForms.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixtemplatesearchvirtualtable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Data/Models/TemplateSearch.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorForms.Data.Models;
+
+public class TemplateSearch
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Comments { get; set; }
+    public string? Tags { get; set; }
+    public string? Fields { get; set; } 
+}
